aa72eb4029c817bfb94eed52ed74af93263516bd
Anurag Verma <anuragvermacoding@gmail.com>
2024-01-25 10:25:35 +0530

    datatypes

---
+++ 02-datatypes.py
@@ -0,0 +1,39 @@
+#Datatypes
+a = 10                                                  #Integer
+b = 2.4                                                  #float
+name = "Anurag"                                          #string
+is_passed = True                                         #Boolean
+_complex = 4+5j                                          #complex
+fruit_list = ['Apple','Banana','Guava','Organes']       #List
+
+#printing varaibles along with thier datatypes
+
+print( a,type(a))
+print( b,type(b))
+print( name,type(name))
+print( is_passed,type(is_passed))
+print( _complex,type(_complex))
+print( fruit_list,type(fruit_list))
+
+#WAP to save Name,Age, YOB,IsNewUser ? and list of fav. food of a user.
+Name = "Anurag Verma"
+Age = 20
+YOB = 2004
+is_New_User = True
+fav_food = [ 'Butter Paneer','Butter Chicken']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
