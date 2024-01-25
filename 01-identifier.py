abcacc4d3c939764177cb4fd8b3589aef190a395
Anurag Verma <anuragvermacoding@gmail.com>
2024-01-25 10:06:11 +0530

    first commit

---
+++ 01-identifier.py
@@ -0,0 +1,17 @@
+# Identifiers 
+name = "Anurag Verma"
+age = 21
+print(name)
+print(age)
+
+# Valid Identifiers
+Apple = 20
+banana = 30
+_cat = 20
+full_name = "Anurag Verma"
+
+# Invalid Identifiers
+ 9name = "Python"           # cannot start with a numerical character
+class = 20                  # cannot use name of a keyword
+fox$ =  90                  # cannot use special character except _
+
