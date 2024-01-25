0be544b0ff5fb9a546a24e84df30d774134972f8
Anurag Verma <anuragvermacoding@gmail.com>
2024-01-25 10:55:18 +0530

    Operators

---
+++ 03-Operators.py
@@ -0,0 +1,97 @@
+#Arithmetic Operators
+a = 5
+b = 4
+
+print(a+b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a*b)
+print(a**b)
+print(a%b)
+
+# WAP to find avg of 3 nos.
+num1 = 20
+num2 = 34
+num3 = 80
+avg = (num1+num2+num3)/3
+print(avg)
+
+#Assignment Operators
+
+p = 7
+p = p+7
+print(p)
+p += 8
+print(p)
+
+
+x = 10          
+print(x)
+x += 5          
+print(x)
+x -= 2          
+print(x)
+x /= 3          
+print(x)
+x //= 2         
+print(x)
+x *= 2          
+print(x)
+x **= 4         
+print(x)
+x %= 4          
+print(x)
+
+#Relational/Comparision Operator
+u = 90
+v = 45
+print(u > v)    #would return ans in either True(1)   or False(0)
+print(u < v)      
+print(u >= v)      
+print(u <= v)      
+print(u == v)      
+print(u != v)      
+
+#Logical Operators
+                    # AND OR NOT
+o = True
+g = False
+
+print("o AND g", o and g)
+print("o or g", o or g)
+print("not g", not g)
+
+#----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
