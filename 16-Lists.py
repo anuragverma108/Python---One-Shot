a25792562738cd54f925ceaca5e2b3f8e2ce4fe7
Anurag Verma <anuragvermacoding@gmail.com>
2024-02-06 13:00:38 +0530

    lists

---
+++ 16-Lists.py
@@ -0,0 +1,18 @@
+l1 = [1,2,3,'Arun','%#']                #heterogeneous in nature
+print(l1)
+      
+fruits = ['apple','banana','guava']
+print(fruits[0])                        #same indexinf fundas as strings
+print(fruits)
+
+#slicing funda is same in list
+#negative slicing funda is also same in list
+
+#lists are mutable
+fruits[0] = 'mango'
+print(fruits)
+
+#deletion of a list element
+
+del fruits[-1]
+print(fruits)
