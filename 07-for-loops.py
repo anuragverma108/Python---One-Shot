ebba07be6aadc4e4b5cfc91cd833e4fc0b5d61fc
Anurag Verma <anuragvermacoding@gmail.com>
2024-02-06 09:55:13 +0530

    loops_in python

---
+++ 07-for-loops.py
@@ -0,0 +1,8 @@
+for x in range(1,11):
+    print(2*x, end=", ") #end bta rha h ki kaise end hoga hr ek element
+
+print("\n")
+
+list1 = ['anurag','amrit','prashant','soham']
+for i in list1:
+    print(3*i, end=" , ")
---
+++ 09-Break_Continue.py
@@ -0,0 +1,18 @@
+#Break
+
+print("Break \n")
+
+for i in range(15):
+    if(i==8):
+        break
+    print(i)
+    
+
+print("Continue \n")
+#continue
+    
+for j in range(15):
+    if(j==8):
+        continue
+    print(j)
+    
